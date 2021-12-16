24ae518935d394e348551dbcdaff0efb61407723
Henrik Saksela <henrik.saksela@gmail.com>
2021-12-17 00:03:09 +0200

    wip

--- src/day16.rs
+++ src/day16.rs
@@ -4,39 +4,92 @@ macro_rules! ones {
     };
 }
 
-pub fn run() -> std::io::Result<()> {
-    let input = std::fs::read_to_string("data/16.txt")?;
-    let data = (0..input.len())
-        .step_by(2)
-        .map(|i| u8::from_str_radix(&input[i..i + 2], 16).unwrap())
-        .collect::<Vec<_>>();
+struct Data(usize, Vec<u8>);
 
-    let mut loc = 0;
-
-    let mut take = |bits: usize| -> u64 {
+impl Data {
+    fn new(data: Vec<u8>) -> Self {
+        Data(0, data)
+    }
+    fn take(&mut self, bits: usize) -> u64 {
         let mut val = 0u64;
-        let offset = loc % 8;
-        let mut curr = data[(loc - offset) / 8] & ones!(8 - offset);
+        let offset = self.0 % 8;
+        let mut curr = self.1[(self.0 - offset) / 8] & ones!(8 - offset);
         let mut read = 8 - offset;
         while read < bits {
             val += curr as u64;
-            curr = data[(loc + read) / 8];
+            curr = self.1[(self.0 + read) / 8];
             read += 8;
             val <<= 8;
         }
-        loc += bits;
+        self.0 += bits;
         (val + curr as u64) >> read - bits
-    };
+    }
+    fn reset(&mut self) {
+        self.0 = 0;
+    }
+}
 
-    // loop {
-    //     let version = take(3);
-    //     let typeid = take(3);
-    // }
+enum Packet {
+    Literal(u8, u64),
+    Operator(u8, u8, Vec<Packet>),
+}
 
-    // println!("{:b}", take(5));
-    // println!("{:b}", take(5));
-    // println!("{:b}", take(5));
-    // println!("{:b}", take(5));
+fn read_packet(data: &mut Data) -> Packet {
+    let version = data.take(3) as u8;
+    let typeid = data.take(3) as u8;
+
+    match typeid {
+        4 => {
+            let mut val = 0u64;
+            loop {
+                let bits = data.take(5);
+                val <<= 4;
+                val += bits & 0b1111;
+                if bits & 1 << 5 != 0 {
+                    break Packet::Literal(version, val);
+                }
+            }
+        }
+        _ => match data.take(1) {
+            0 => {
+                let len = data.take(15) as usize;
+                let start = data.0;
+                let mut packets = vec![];
+                loop {
+                    if len == data.0 - start {
+                        break Packet::Operator(version, typeid, packets);
+                    }
+                    packets.push(read_packet(data));
+                }
+            }
+            1 => {
+                let len = data.take(11);
+                let mut packets = vec![];
+                for _ in 0..len {
+                    packets.push(read_packet(data));
+                }
+                Packet::Operator(version, typeid, packets)
+            }
+            _ => panic!(),
+        },
+    }
+}
+
+pub fn run() -> std::io::Result<()> {
+    let input = std::fs::read_to_string("data/16.txt")?;
+    let mut data = Data::new(
+        (0..input.len())
+            .step_by(2)
+            .map(|i| u8::from_str_radix(&input[i..i + 2], 16).unwrap())
+            .collect::<Vec<_>>(),
+    );
+
+    let root = read_packet(&mut data);
+
+    // println!("{:b}", data.take(5));
+    // println!("{:b}", data.take(5));
+    // println!("{:b}", data.take(5));
+    // println!("{:b}", data.take(5));
 
     println!("puzzle 15.1 {}", "solve(&grid, 1)");
 
